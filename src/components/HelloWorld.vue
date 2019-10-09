2de7ce56f1fcb2e07a00b55cfdebbcce6a6082b9
Brian Bender <endlessrevolt@gmail.com>
2019-10-09 16:57:59 -0700

    Demonstration of scss issue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,10 +39,12 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" module>
+@import "../Globals";
+
 h3 {
   margin: 40px 0 0;
+  font: 700 $fontSizeLarge $accentFontFamily;
 }
 ul {
   list-style-type: none;
