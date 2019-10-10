31f51d72ab0d2400910f02d6b9f2831f35a32a85
Brian Bender <endlessrevolt@gmail.com>
2019-10-09 17:25:42 -0700

    Simplified HelloWorld component, add unit test demonstrating the flaw

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -9,4 +9,12 @@ describe('HelloWorld.vue', () => {
     })
     expect(wrapper.text()).toMatch(msg)
   })
+
+  it('has the correct scss class', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.html()).toContain('class="title"')
+  });
 })
